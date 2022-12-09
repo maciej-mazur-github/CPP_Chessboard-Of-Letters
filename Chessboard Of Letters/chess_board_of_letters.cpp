8b3d89a0b7681b2503c7b16343b224548108c992
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-12-09 19:47:05 +0100

    Implement fieldCreator()

--- Chessboard Of Letters/chess_board_of_letters.cpp
+++ Chessboard Of Letters/chess_board_of_letters.cpp
@@ -6,7 +6,7 @@
 
 void ChessBoardOfLetters::chessBoardCreator()
 {
-	chessBoardPtr = new char* [CHESS_BOARD_SIZE];
+	chessBoardPtr = new char* [CHESS_BOARD_SIZE];   // dynamically allocate memory for array of pointers char arrays also dynamically allocated below, making it 8x8 board as in chess board
 
 	for (int i = 0; i < CHESS_BOARD_SIZE; i++)
 	{
@@ -17,4 +17,11 @@ void ChessBoardOfLetters::chessBoardCreator()
 			chessBoardPtr[i][j] = '_';
 		}
 	}
+}
+
+
+
+void ChessBoardOfLetters::fieldCreator()
+{
+	fieldsPtr = new char* [CHESS_BOARD_SIZE * CHESS_BOARD_SIZE];    // dynamic allocation of memory for 64-element array of pointers to 2-element arrays (future field coordinates, i.e. a2, b7, d8 etc.)
 }
