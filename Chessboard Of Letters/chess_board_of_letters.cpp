103e0cda42bf36ae8b80e3564c748e13999260d5
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-12-09 20:24:50 +0100

    Implement printChessBoard()

--- Chessboard Of Letters/chess_board_of_letters.cpp
+++ Chessboard Of Letters/chess_board_of_letters.cpp
@@ -4,6 +4,34 @@
 #include <conio.h>
 #include <stdlib.h>
 
+
+
+ChessBoardOfLetters::ChessBoardOfLetters()
+{
+	chessBoardCreator();
+	fieldCreator();
+	figureCreator();
+}
+
+
+void ChessBoardOfLetters::printChessBoard()
+{
+	cout << endl;
+
+	for (int i = CHESS_BOARD_SIZE - 1; i >= 0; i--)
+	{
+		cout << "\t";
+
+		for (int j = 0; j < CHESS_BOARD_SIZE; j++)
+		{
+			cout << chessBoardPtr[i][j] << "\t";
+		}
+
+		cout << endl << endl << endl;
+	}
+}
+
+
 void ChessBoardOfLetters::chessBoardCreator()
 {
 	chessBoardPtr = new char* [CHESS_BOARD_SIZE];   // dynamically allocate memory for array of pointers char arrays also dynamically allocated below, making it 8x8 board as in chess board
@@ -23,5 +51,23 @@ void ChessBoardOfLetters::chessBoardCreator()
 
 void ChessBoardOfLetters::fieldCreator()
 {
-	fieldsPtr = new char* [CHESS_BOARD_SIZE * CHESS_BOARD_SIZE];    // dynamic allocation of memory for 64-element array of pointers to 2-element arrays (future field coordinates, i.e. a2, b7, d8 etc.)
+	int fieldNumber = CHESS_BOARD_SIZE * CHESS_BOARD_SIZE;
+	fieldsPtr = new char* [fieldNumber];    // dynamic allocation of memory for 64-element array of pointers to 2-element arrays (future field coordinates, i.e. a2, b7, d8 etc.)
+
+	for (int i = 0; i < fieldNumber; i++)
+	{
+		fieldsPtr[i] = new char[2];
+	}
+}
+
+
+void ChessBoardOfLetters::figureCreator()
+{
+	int figureNumber = CHESS_BOARD_SIZE * CHESS_BOARD_SIZE;
+	figuresPtr = new char* [figureNumber];
+
+	for (int i = 0; i < figureNumber; i++)
+	{
+		figuresPtr[i] = nullptr;
+	}
 }
--- Chessboard Of Letters/chess_board_of_letters.h
+++ Chessboard Of Letters/chess_board_of_letters.h
@@ -15,6 +15,6 @@ public:
 	ChessBoardOfLetters();
 	void insertFigures();
 	void printChessBoard();
-	~ChessBoardOfLetters();
+	//~ChessBoardOfLetters();
 };
 
